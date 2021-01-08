b8ae599d83eca775cb0baac71b13ba39231f8a31
Markus Seidler <mseidler79@gmail.com>
2021-01-08 23:30:03 +0800

    Initial Commit

---
+++ Memorize2021/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize2021
+//
+//  Created by Markus Seidler on 8/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Memorize2021/Memorize2021App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize2021App.swift
+//  Memorize2021
+//
+//  Created by Markus Seidler on 8/1/21.
+//
+
+import SwiftUI
+
+@main
+struct Memorize2021App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
